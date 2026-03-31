02870596070205f1a981acb0ff8362cd4c3edb02
Philv17 <philv17@gmail.com>
2026-03-31 13:06:13 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CallMattWebsite from './CallMattWebsite.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CallMattWebsite />
+  </React.StrictMode>,
+)
